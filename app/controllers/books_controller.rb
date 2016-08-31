c4fa6f7d19eca58228cd9e7c78fbb9b4340235ae
appwebtech <josembi@gmail.com>
2016-08-31 21:06:48 +0200

    Design category drop-down on nav-bar to un-fuck mumbo jumbo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,12 @@ class BooksController < ApplicationController
 	before_action :tafuta_book, only: [:show, :edit, :update, :destroy]
 
 	def index
+		if params[:category].blank?
 		@books = Book.all.order("created_at DESC")
+	else
+		@category_id = Category.find_by(name: params[:category]).id
+		@books = Book.where(:category_id => @category_id).order("created_at DESC")
+	end
 	end
 
 	def show
